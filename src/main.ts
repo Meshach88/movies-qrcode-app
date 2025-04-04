5dc675c858a8c65e40c3850e0271876090de8337
Meshach Esuabom <meshach.esuabom@iisysgroup.com>
2025-04-04 01:55:12 +0100

    changed frontend url in main.ts to vercel url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "https://movies-qrcode-app.onrender.com", // Allow frontend requests
+    origin: "https://movies-qrcode-client.vercel.app", // Allow frontend requests
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
