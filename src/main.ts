7d45f7f61f9b6988c512d28384d80fbf70c36be9
Meshach Esuabom <meshach.esuabom@iisysgroup.com>
2025-04-04 01:22:51 +0100

    edited cors in main.ts to allow request from deployed frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:5173", // Allow frontend requests
+    origin: "https://movies-qrcode-app.onrender.com", // Allow frontend requests
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
